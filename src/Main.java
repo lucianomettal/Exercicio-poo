074c2b1e627755c8b8e46ccaa9e11eebc787a9d8
lucianomettal <luciano.andrade@aluno.imepac.edu.br>
2025-02-27 21:13:58 -0300

    Adicionando soma

--- src/Main.java
+++ src/Main.java
@@ -5,5 +5,15 @@ public class Main {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
         System.out.printf("Hello and welcome!");
+
+        int num1 = 10; // Primeiro número
+        int num2 = 20; // Segundo número
+
+        // Soma dos dois números
+        int soma = num1 + num2;
+
+        // Exibe o resultado
+        System.out.println("A soma de " + num1 + " e " + num2 + " é: " + soma);
     }
 }
+
